4ad7263d2321e0e639358164367278b6b09698d4
ILoveEveryone24 <ernestascizas2404@gmail.com>
2024-10-28 19:34:13 +0100

    PythonSuite

--- http_proxy/main.py
+++ http_proxy/main.py
@@ -1,11 +1,33 @@
 import socket
 import sys
+import subprocess
+import tempfile
+import os
+
+def edit_request(request_data):
+    request_str = request_data.decode("utf-8")
+
+    with tempfile.NamedTemporaryFile(suffix=".txt", mode="w+", delete=False) as temp_file:
+        temp_file.write(request_str)
+        temp_file.flush()
+
+        editor = os.getenv("EDITOR", "vim")
+        subprocess.call([editor, temp_file.name])
+
+        temp_file.seek(0)
+        edited_data = temp_file.read()
+
+    os.unlink(temp_file.name)
+
+    return edited_data
 
 def handle_client(client_socket):
     request = client_socket.recv(1024)
     print(f"[RECEIVED] {request.decode()}")
     target_server_addr = sys.argv[1]
     target_server_port = int(sys.argv[2])
+
+    edit_request(request) 
 
     target_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     target_socket.connect((target_server_addr, target_server_port))
